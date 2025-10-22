c034fde0b2264b14cd31ac243a714f7593968b74
fpwilliams71 <fpwilliams71@yahoo.com>
2025-10-22 19:35:09 +0100

    Add AWS resources for S3 bucket and CloudFront

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,1 +1,67 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_s3_bucket" "website_bucket" {
+  bucket = "secure-devops-lab-demo-${random_id.suffix.hex}"
+  acl    = "public-read"
+
+  website {
+    index_document = "index.html"
+  }
+
+  tags = {
+    Name = "SecureDevOpsWebsite"
+  }
+}
+
+resource "random_id" "suffix" {
+  byte_length = 4
+}
+
+resource "aws_s3_bucket_policy" "public_read" {
+  bucket = aws_s3_bucket.website_bucket.id
+  policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Effect = "Allow",
+        Principal = "*",
+        Action = ["s3:GetObject"],
+        Resource = "${aws_s3_bucket.website_bucket.arn}/*"
+      }
+    ]
+  })
+}
+
+resource "aws_cloudfront_distribution" "cdn" {
+  origin {
+    domain_name = "${aws_s3_bucket.website_bucket.bucket_regional_domain_name}"
+    origin_id   = "s3-origin"
+  }
+
+  enabled             = true
+  default_root_object = "index.html"
+
+  default_cache_behavior {
+    allowed_methods  = ["GET", "HEAD"]
+    cached_methods   = ["GET", "HEAD"]
+    target_origin_id = "s3-origin"
+    viewer_protocol_policy = "redirect-to-https"
+  }
+
+  viewer_certificate {
+    cloudfront_default_certificate = true
+  }
+
+  restrictions {
+    geo_restriction {
+      restriction_type = "none"
+    }
+  }
+
+  tags = {
+    Name = "SecureDevOpsCDN"
+  }
+}
 
